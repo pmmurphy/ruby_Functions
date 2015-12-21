d4135d9912a2a4eb9ec43e5e462681e32687dff2
Paula Murphy <paulam849@gmail.com>
2015-12-20 20:56:53 -0500

    inital commit

---
+++ ruby_basics_2.rb
@@ -0,0 +1,41 @@
+#Create a function that takes a string and adds the phrase "Only in America!" to the end of it.
+
+# def york(a)
+
+# 	puts a + " Only in America"
+# end	
+# york("i love this , ")
+
+# Create a function to find the maximum value in an array of numbers. For instance: [100,10,-1000] should return 100.
+
+# def max_val(dummy)
+# 	print dummy.max
+# end
+# max_val([100, 50])
+
+# 	Create a function that takes two arguments - both of them arrays. Inside of the function, combine the arrays using the items from the first array as keys and the second array as values into a hash. For example, when these two arrays are supplied as arguments:
+
+# ruby [:toyota, :tesla] ["Prius", "Model S"]
+# they should return a hash like so:
+# ruby {toyota: "Prius", tesla: "Model S"} 
+
+# array1 = ["peas", "cheese"]
+# array2 = ["deer", "cow"]
+ 
+# def bunny(cat)
+	bat = Hash.new
+	cat.each do |i|
+		puts i
+ end
+end	
+carrot(array1)
+
+# 	
+#  	
+
+# end	
+
+# Fizzbuzz
+
+def fizzbuzz()
+	for i 0..100
